58e92a2c58fcc97cfd58ea5d3e4bf21bc54147f5
Michael-Njoroge <mikethecoder12@gmail.com>
2023-12-11 11:18:07 +0300

    remember last selected tab after refreshing the page

--- script.js
+++ script.js
@@ -33,9 +33,13 @@ function updateRowNumbers() {
 
 // FUNCTION TO HANDLE TABLE DISPLAY BASED ON SELECTED RADIO BUTTON
 $(document).ready(function() {
+
     // Handle click events for radio buttons
     $('input[name="sidebar_tab1"]').on('change', function() {
       var value = $(this).val();
+
+      // Store the selected tab in localStorage
+      localStorage.setItem('lastSelectedTab', value);
 
       // Hide all tables
       $('.table-container').hide();
@@ -48,6 +52,21 @@ $(document).ready(function() {
       }
       // Add more conditions for other radio buttons  
     });
+
+    // Retrieve the last selected radio button value from local storage
+    var lastSelectedTab = localStorage.getItem('lastSelectedTab');
+
+     // Set the default value if no value is found in local storage
+     if (!lastSelectedTab) {
+      lastSelectedTab = '1';  
+    }
+
+     // Set the last selected radio button as checked
+     $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').prop('checked', true);
+
+     // Trigger the change event to display the corresponding table
+     $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').change();
+ 
   });
 
 
