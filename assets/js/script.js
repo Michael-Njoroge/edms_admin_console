2f22f194898498fab96467ebced58b73afa07f9a
Michael-Njoroge <mikethecoder12@gmail.com>
2024-01-04 12:58:17 +0300

    corrections on sidebar functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,52 @@ function updateRowNumbers() {
 }
 
  
+//******************************** SIDEBAR FUNCTIONS *********************************//
+
+
+
+
+        //**************************** NAVBAR FUNCTIONS*************************//
+
+
+// FUNCTION TO FETCH USER INFORMATION BASED ON THE LOGGED-IN USER'S ID
+function fetchUserInfoAndUpdateNavbar() {
+    // Retrieve the Bearer token from localStorage
+    const bearerToken = localStorage.getItem('edms_token');
+
+    // Check if the token is present in localStorage
+    if (!bearerToken) {
+        console.error('Unauthorized');
+        return;
+    }
+
+    // Decode the JWT to extract the user ID
+    const decodedToken = parseJwt(bearerToken);
+    const userId = decodedToken.sub; // Assuming 'sub' contains the user ID
  
+    // Make a GET request to fetch user information based on the user's ID
+    fetch(`http://127.0.0.1:8000/api/user/show/${userId}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${bearerToken}`,
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Update the user's name in the navigation bar
+        updateUserNameInNavbar(data.data.data.username);
+
+         // Update the profile modal
+         updateProfileModal(data.data.data);
+ 
+        // Call populateDropdowns once user information is retrieved
+        populateDropdowns();
+    })
+    .catch(error => {
+        console.error('Error fetching user information:', error);
+    });
+}
 
 // FUNCTION TO UPDATE THE USER'S NAME IN THE NAVIGATION BAR
 function updateUserNameInNavbar(userName) {
--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,43 +1,41 @@
-//******************************** SIDEBAR FUNCTIONS *********************************//
-
 // FUNCTION TO HANDLE TABLE DISPLAY BASED ON SELECTED RADIO BUTTON
 $(document).ready(function() {
 
-    // Handle click events for radio buttons
-    $('input[name="sidebar_tab1"]').on('change', function() {
-      var value = $(this).val();
+  // Handle click events for radio buttons
+  $('input[name="sidebar_tab1"]').on('change', function() {
+    var value = $(this).val();
 
-      // Store the selected tab in localStorage
-      localStorage.setItem('lastSelectedTab', value);
+    // Store the selected tab in localStorage
+    localStorage.setItem('lastSelectedTab', value);
 
-      // Hide all tables
-      $('.table-container').hide();
+    // Hide all tables
+    $('.table-container').hide();
 
-      // Show the corresponding table based on the selected radio button
-      if (value === '3') {
-        $('#groupsTableContainer').show();
-      } else if (value === '4') {
-        $('#groupMembershipsTableContainer').show();
-      }else if (value === '5') {
-        $('#groupPermissionsTableContainer').show();
-      }else if (value === '6') {
-        $('#usersTableContainer').show();
-      }
-      // Add more conditions for other radio buttons  
-    });
-
-    // Retrieve the last selected radio button value from local storage
-    var lastSelectedTab = localStorage.getItem('lastSelectedTab');
-
-     // Set the default value if no value is found in local storage
-     if (!lastSelectedTab) {
-      lastSelectedTab = '1';  
+    // Show the corresponding table based on the selected radio button
+    if (value === '3') {
+      $('#groupsTableContainer').show();
+    } else if (value === '4') {
+      $('#groupMembershipsTableContainer').show();
+    }else if (value === '5') {
+      $('#groupPermissionsTableContainer').show();
+    }else if (value === '6') {
+      $('#usersTableContainer').show();
     }
-
-     // Set the last selected radio button as checked
-     $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').prop('checked', true);
-
-     // Trigger the change event to display the corresponding table
-     $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').change();
- 
+    // Add more conditions for other radio buttons  
   });
+
+  // Retrieve the last selected radio button value from local storage
+  var lastSelectedTab = localStorage.getItem('lastSelectedTab');
+
+   // Set the default value if no value is found in local storage
+   if (!lastSelectedTab) {
+    lastSelectedTab = '1';  
+  }
+
+   // Set the last selected radio button as checked
+   $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').prop('checked', true);
+
+   // Trigger the change event to display the corresponding table
+   $('input[name="sidebar_tab1"][value="' + lastSelectedTab + '"]').change();
+
+});
