9946cab6cc808ac1417cd28d583ee36c805516ef
Michael-Njoroge <mikethecoder12@gmail.com>
2024-01-04 13:40:40 +0300

    complete the group membership functions

---
+++ assets/js/groupMemberships.js
@@ -0,0 +1,214 @@
+// ****************************GROUP MEMBERSHIPS FUNCTIONS************************ //
+async function getGroupMembershipsData(page = 1, itemsPerPage = 5) {
+  // Retrieve the Bearer token from localStorage
+  const bearerToken = localStorage.getItem("edms_token");
+
+  // Fetch group memberships data with pagination parameters
+  const membershipsResponse = await fetch(
+    `http://127.0.0.1:8000/api/groupmemberships?page=${page}&itemsPerPage=${itemsPerPage}`,
+    {
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const membershipsData = await membershipsResponse.json();
+
+  // Get the table element
+  const membershipsTable = $("#groupMembershipsTab").DataTable({
+    lengthMenu: [5, 10, 25, 50],
+    destroy: true,
+  });
+
+  // Clear the existing table rows
+  membershipsTable.clear().draw();
+
+  // Function to fetch user data based on user_id
+  const fetchUserData = async (userId) => {
+    const userResponse = await fetch(
+      `http://127.0.0.1:8000/api/user/show/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${bearerToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const userData = await userResponse.json();
+    return userData.data.data.username;
+  };
+
+  // Function to fetch group data based on group_id
+  const fetchGroupData = async (groupId) => {
+    const groupResponse = await fetch(
+      `http://127.0.0.1:8000/api/group/show/${groupId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${bearerToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const groupData = await groupResponse.json();
+    return groupData.data.data.group_name;
+  };
+
+  // Iterate through the data and append rows to the DataTable
+  for (let index = 0; index < membershipsData.data.data.length; index++) {
+    const membership = membershipsData.data.data[index];
+    const username = await fetchUserData(membership.user_id);
+    const groupName = await fetchGroupData(membership.group_id);
+
+    membershipsTable.row
+      .add([
+        index + 1,
+        username,
+        groupName,
+        `<a href="#" onclick="confirmDeleteGroupMembership(${membership.id})" title="delete"><i class="fa fa-trash"></i></a>`,
+      ])
+      .draw(false);
+  }
+
+  // Call updateRowNumbers after appending rows to the table body
+  updateRowNumbers();
+
+  // Generate pagination links
+  const totalPages = Math.ceil(membershipsData.data.total / itemsPerPage);
+  const paginationElement = $("#membershipsPagination");
+  paginationElement.empty();
+
+  if (totalPages > 1) {
+    const prevLink = `<a href="#" onclick="getGroupMembershipsData(${
+      page - 1
+    }, ${itemsPerPage})" class="page-link">«</a>`;
+    paginationElement.append(prevLink);
+
+    for (let i = 1; i <= totalPages; i++) {
+      const pageLink = `<a href="#" onclick="getGroupMembershipsData(${i}, ${itemsPerPage})" class="page-link ${
+        i === page ? "active" : ""
+      }">${i}</a>`;
+      paginationElement.append(pageLink);
+    }
+
+    const nextLink = `<a href="#" onclick="getGroupMembershipsData(${
+      page + 1
+    }, ${itemsPerPage})" class="page-link">»</a>`;
+    paginationElement.append(nextLink);
+  }
+}
+
+// FUNCTION TO HANDLE FORM SUBMISSION AND ASSIGN USERS TO A GROUP
+function assignUsersToGroup() {
+  // Fetch form data
+  const groupId = $("#assign_group").val();
+  const userIds = $("#assign_users").val();
+
+  // Check if both group and user options are selected
+  if (!groupId || !userIds || userIds.length === 0) {
+    toastr.error("Please select both a group and at least one user.");
+    return;
+  }
+
+  // Disable the submit button and show loading text
+  const submitButton = $("#assignUsersBtn");
+  submitButton
+    .prop("disabled", true)
+    .html('<i class="fa fa-spinner fa-spin"></i> Assigning...');
+
+  // Construct the request payload
+  const requestData = {
+    group_id: groupId,
+    user_id: userIds,
+  };
+
+  // Retrieve the Bearer token from localStorage
+  const bearerToken = localStorage.getItem("edms_token");
+
+  // Make a POST request to assign group to a group permissions
+  fetch("http://127.0.0.1:8000/api/groupmemberships/store", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Close the modal after successful submission
+      $("#assignUsersModal").modal("hide");
+      $("#assignUsersForm")[0].reset();
+
+      toastr.success("User Assigned to Group successfully");
+
+      // Refresh the data by calling the getData() function
+      getData();
+
+      getGroupMembershipsData();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    })
+    .finally(() => {
+      // Enable the submit button and revert the text
+      submitButton.prop("disabled", false).html("Assign Users");
+    });
+}
+
+// FUNCTION TO HANDLE THE CLICK EVENT OF THE DELETE BUTTON IN EACH ROW
+function confirmDeleteGroupMembership(membershipId) {
+  // Set the membership ID in the hidden input field of the confirmation modal
+  document.getElementById("deleteMembershipId").value = membershipId;
+
+  // Show the confirmation modal
+  $("#confirmDeleteMmebershipModal").modal("show");
+}
+
+// Add this function to handle the click event of the delete button in the confirmation modal
+async function deleteGroupMembership() {
+  // Retrieve the membership ID from the hidden input field
+  const membershipId = document.getElementById("deleteMembershipId").value;
+
+  try {
+    // Disable the delete button
+    const deleteButton = $("#deleteGroupMembershipBtn");
+    deleteButton.prop("disabled", true);
+
+    // Retrieve the Bearer token from localStorage
+    const bearerToken = localStorage.getItem("edms_token");
+
+    // Perform the API call to delete the group membership
+    const apiUrl = `http://127.0.0.1:8000/api/groupmemberships/delete/${membershipId}`;
+
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${bearerToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    // Handle the response, update the UI, or show a success message
+    console.log("Group membership deleted successfully:", data);
+
+    // Close the confirmation modal
+    $("#confirmDeleteMmebershipModal").modal("hide");
+
+    toastr.success("Group Membership Deleted Successfully");
+
+    // Refresh the table or update the UI as needed
+    getGroupMembershipsData();
+
+    // Refresh the data by calling the getData() function
+    getData();
+  } catch (error) {
+    console.error("Error deleting group membership:", error);
+
+    // Close the confirmation modal
+    $("#confirmDeleteMmebershipModal").modal("hide");
+  }
+}
--- assets/js/usersTable.js
+++ assets/js/usersTable.js
@@ -1,72 +1,77 @@
- 
 ///////// FUNCTION TO POPULATE THE USERS TABLE WITH FETCHED USER DATA //////////
 
 async function populateUsersTable(page = 1, itemsPerPage = 5) {
-    // Retrieve the Bearer token from localStorage
-    const bearerToken = localStorage.getItem('edms_token');
-  
-    // Check if the token is present in localStorage
-    if (!bearerToken) {
-      console.error('Unauthorized');
-      return;
-    }
-  
-    // Fetch users with pagination parameters
-    const records = await fetch('http://127.0.0.1:8000/api/users', {
-      headers: {
-        'Authorization': `Bearer ${bearerToken}`,
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    const data = await records.json();
-  
-    // Initialize DataTable
-    const usersTable = $('#usersTable').DataTable({
-      lengthMenu: [5, 10, 25, 50],
-      "bDestroy": true
-    }); 
-   
-  
-    // Loop through each user and create a table row
-    data.data.data.forEach((user, index) => {
-      // Check if the user has a photo
-      const photoUrl = user.photo ? user.photo : '../images/no_image.jpg';
-  
-      // Check the status and set the corresponding label and color
-      const statusLabel = user.status === 'active' ? 'Active' : 'InActive';
-      const statusColor = user.status === 'active' ? 'green' : 'red';
-  
-      // Add the row to DataTable
-      usersTable.row.add([
+  // Retrieve the Bearer token from localStorage
+  const bearerToken = localStorage.getItem("edms_token");
+
+  // Check if the token is present in localStorage
+  if (!bearerToken) {
+    console.error("Unauthorized");
+    return;
+  }
+
+  // Fetch users with pagination parameters
+  const records = await fetch("http://127.0.0.1:8000/api/users", {
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await records.json();
+
+  // Initialize DataTable
+  const usersTable = $("#usersTable").DataTable({
+    lengthMenu: [5, 10, 25, 50],
+    bDestroy: true,
+  });
+
+  // Loop through each user and create a table row
+  data.data.data.forEach((user, index) => {
+    // Check if the user has a photo
+    const photoUrl = user.photo ? user.photo : "../images/no_image.jpg";
+
+    // Check the status and set the corresponding label and color
+    const statusLabel = user.status === "active" ? "Active" : "InActive";
+    const statusColor = user.status === "active" ? "green" : "red";
+
+    // Add the row to DataTable
+    usersTable.row
+      .add([
         index + 1,
         user.name,
         user.username,
         `<img src="${photoUrl}" alt="User Photo" class="user-photo" style="width: 40px; height: 40px; border-radius: 50%;" />`,
-        'stamp',
-        'signature',
+        "stamp",
+        "signature",
         `<span style="color: ${statusColor};">${statusLabel}</span>`,
         `<a href="#" onclick="performUserAction('${user.username}', 'activate')" style="margin-left: 20px;"><i class="fa fa-check" title="Activate" style="color: green; font-size: 24px;"></i></a>
-           <a href="#" onclick="performUserAction('${user.username}', 'deactivate')" style="margin-left: 40px;"><i class="fa fa-ban" title="Deactivate" style="color: red; font-size: 24px; "></i></a>`
-      ]).draw(false);
-    });
-  
-    // Generate pagination links
-    const totalPages = Math.ceil(data.data.total / itemsPerPage);
-    const paginationElement = $('#usersPagination');
-    paginationElement.empty();
-  
-    if (totalPages > 1) {
-      const prevLink = `<a href="#" onclick="populateUsersTable(${page - 1}, ${itemsPerPage})" class="page-link">«</a>`;
-      paginationElement.append(prevLink);
-  
-      for (let i = 1; i <= totalPages; i++) {
-        const pageLink = `<a href="#" onclick="populateUsersTable(${i}, ${itemsPerPage})" class="page-link ${i === page ? 'active' : ''}">${i}</a>`;
-        paginationElement.append(pageLink);
-      }
-  
-      const nextLink = `<a href="#" onclick="populateUsersTable(${page + 1}, ${itemsPerPage})" class="page-link">»</a>`;
-      paginationElement.append(nextLink);
+           <a href="#" onclick="performUserAction('${user.username}', 'deactivate')" style="margin-left: 40px;"><i class="fa fa-ban" title="Deactivate" style="color: red; font-size: 24px; "></i></a>`,
+      ])
+      .draw(false);
+  });
+
+  // Generate pagination links
+  const totalPages = Math.ceil(data.data.total / itemsPerPage);
+  const paginationElement = $("#usersPagination");
+  paginationElement.empty();
+
+  if (totalPages > 1) {
+    const prevLink = `<a href="#" onclick="populateUsersTable(${
+      page - 1
+    }, ${itemsPerPage})" class="page-link">«</a>`;
+    paginationElement.append(prevLink);
+
+    for (let i = 1; i <= totalPages; i++) {
+      const pageLink = `<a href="#" onclick="populateUsersTable(${i}, ${itemsPerPage})" class="page-link ${
+        i === page ? "active" : ""
+      }">${i}</a>`;
+      paginationElement.append(pageLink);
     }
+
+    const nextLink = `<a href="#" onclick="populateUsersTable(${
+      page + 1
+    }, ${itemsPerPage})" class="page-link">»</a>`;
+    paginationElement.append(nextLink);
   }
-  
+}
