7d3db6e060e1fb654153f18e6043ec1561d0e431
Michael-Njoroge <mikethecoder12@gmail.com>
2024-01-07 17:40:47 +0300

    minor corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,19 +88,3 @@ async function fetchDataAndPopulateCounts() {
 
 // Call the async function
 fetchDataAndPopulateCounts();
-
-$(document).ready(function () {
-  /* Uses counterup.js */
-  $(".counter").counterUp({
-    delay: 20,
-    time: 2000,
-  });
-
-  /* Uses waypoints.js */
-  $(".graph").waypoint(
-    function () {
-      $(".graph").addClass("grow");
-    },
-    { offset: "100%" }
-  );
-});
